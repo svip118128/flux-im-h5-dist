b8783ccda8ece2987c21fec19bd6ba03ea5db40d
kiet <skill1sp2@gmail.com>
2026-02-04 16:55:52 +0700

    update version meta tag

---
+++ dist/assets/useFontSize-0df10a9a.js
@@ -0,0 +1,1 @@
+import{aN as e,aO as a}from"./main-22c3e62d.js";import{r as n}from"./@vue-48d528d1.js";const t=n(1);function z(){return{fontSizeFactor:t,loadFontSize:()=>{t.value=e()},saveFontSize:o=>{a(o),t.value=o}}}export{z as u};
